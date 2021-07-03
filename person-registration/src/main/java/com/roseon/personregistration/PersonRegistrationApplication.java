530f3b97e6b9aeeab132df585b7258801a1eb173
Bianca <66906291+Biafcampos@users.noreply.github.com>
2021-07-03 20:09:16 -0300

    Add files

---
+++ person-registration/src/main/java/com/roseon/personregistration/PersonRegistrationApplication.java
@@ -0,0 +1,13 @@
+package com.roseon.personregistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonRegistrationApplication.class, args);
+	}
+
+}
